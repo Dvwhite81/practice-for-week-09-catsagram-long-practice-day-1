54e977c43149e767465f369df685fa505da2e8c2
Dan <dvwhite1981@gmail.com>
2023-08-16 12:08:29 -0400

    Day 1 complete

--- public/index.js
+++ public/index.js
@@ -1,1 +1,41 @@
-// Your code here
+const createContainer = () => {
+    const container = document.createElement("div");
+    container.classList.add("container");
+
+    return container;
+}
+const createTitle = (container) => {
+    const heading = document.createElement("h1");
+    heading.id = "main-title";
+    heading.textContent = "Kitten Pic";
+
+    container.append(heading);
+    return container;
+};
+
+const createPicture = (container, url) => {
+    const img = document.createElement("img");
+    img.id = "main-img";
+    img.src = url;
+
+    container.append(img);
+    return container;
+};
+
+const getUrl = () => {
+    return fetch("https://api.thecatapi.com/v1/images/search")
+        .then(res => res.json())
+        .then(data => {
+            return data[0].url;
+        });
+}
+
+async function main() {
+    const container = createContainer();
+    createTitle(container);
+    const url = await getUrl();
+    createPicture(container, url);
+    document.body.append(container);
+};
+
+window.onload = main();
